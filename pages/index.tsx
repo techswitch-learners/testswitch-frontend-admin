585fd9d645310ab595326503304151ac01300fcc
Beatrice <beatrice.opara@softwire.com>
2020-04-14 16:44:43 +0100

    Added SCSS to project
    Installed SCSS for typescript

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 ﻿import {NextPage} from "next";
+import scss from '../page-styles/index.module.scss'
 
-const Home: NextPage = () => <h1>Hello Admin</h1>;
+const Home: NextPage = () => <h1 className={scss.example}>Hello Admin</h1>;
 
 export default Home;
