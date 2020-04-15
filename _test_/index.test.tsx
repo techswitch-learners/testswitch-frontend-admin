37fbbde5fe9b8c361435aa83209f27221731a281
hnalabanda <hemanalabanda@gmail.com>
2020-04-15 15:02:55 +0100

    Added a test and a changes to package.json file to run the tests

---
+++ _test_/index.test.tsx
@@ -0,0 +1,7 @@
+﻿import React from 'react';
+import {render} from '@testing-library/react';
+import Home from "../pages";
+
+test('Check that the home page renders',()=>{
+    let home=render(<Home/>);
+});
