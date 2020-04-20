59366e8b2e23102362d00a92252003cc65e58e0f
Jamie Baird <james.baird@softwire.com>
2020-04-20 12:18:04 +0100

    Created Login form component

---
+++ components/LoginForm.tsx
@@ -0,0 +1,30 @@
+﻿import React, {useContext, useState} from "react";
+import {NextPage} from "next";
+import scss from "../pageStyles/index.module.scss"
+
+
+interface LoginFormProps {
+    userId : string;
+    password: string;
+    setUserId : Function;
+    setPassword : Function;
+}
+
+export function LoginForm(props: LoginFormProps): JSX.Element {
+   
+    return (
+            <form>
+                <label>
+                    User Id:
+                    <input type={"text"} value={props.userId}
+                           onChange={event => props.setUserId(event.target.value)}/>
+                </label>
+                <label>
+                    Password:
+                    <input type={"password"} value={props.password} onChange={event => props.setPassword(event.target.value)}/>
+                </label>
+
+                <button type="submit">Log In</button>
+            </form>
+    );
+}
