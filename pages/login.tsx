a7b405466f9a16bb0748d4e6596f0bdf3e658f89
Jamie Baird <james.baird@softwire.com>
2020-04-20 11:20:27 +0100

    Added the basic components of the Login page - no styling

---
+++ pages/login.tsx
@@ -0,0 +1,19 @@
+﻿import React from "react";
+import {NextPage} from "next";
+import scss from "../pageStyles/index.module.scss"
+
+
+const LogIn: NextPage = () => 
+    <div>
+        <h1 className={scss.example}>Candidate Manager</h1>
+        <h2>Administrator Login</h2>
+        <hr/>
+        <h2>User ID</h2>
+        <input/>
+        <h2>Password</h2>
+        <input/>
+        <br/><br/>
+        <button>Login</button>
+    </div>;
+    
+export default LogIn;
