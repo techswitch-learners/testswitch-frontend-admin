b835e7d0bda1e2e07d46e531b9a52a931105068c
Jamie Baird <james.baird@softwire.com>
2020-04-20 12:18:57 +0100

    Switched to using form component.
    Also added useState for userId and password

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,19 +1,28 @@
-﻿import React from "react";
+﻿import {LoginForm} from "../components/LoginForm";
+import React, {useContext, useState} from "react";
 import {NextPage} from "next";
 import scss from "../pageStyles/index.module.scss"
 
 
-const LogIn: NextPage = () => 
-    <div>
-        <h1 className={scss.example}>Candidate Manager</h1>
-        <h2>Administrator Login</h2>
-        <hr/>
-        <h2>User ID</h2>
-        <input/>
-        <h2>Password</h2>
-        <input/>
-        <br/><br/>
-        <button>Login</button>
-    </div>;
-    
-export default LogIn;
+
+export default function Login(): JSX.Element {
+    // const loginContext = useContext(LoginContext);
+
+    const [userId, setUserId] = useState("");
+    const [password, setPassword] = useState("");
+
+
+    return (
+        <div>
+            <h1 className={scss.example}>Candidate Manager</h1>
+            <h2>Administrator Login</h2>
+            <hr/>
+            <LoginForm 
+                userId={userId} 
+                password={password} 
+                setUserId={setUserId} 
+                setPassword={setPassword}/>
+
+        </div>
+    );
+}
