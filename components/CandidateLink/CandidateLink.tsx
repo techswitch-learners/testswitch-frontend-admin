7d5273dcea95e05c908b6bb35339266984f7c665
Jamie Baird <james.baird@softwire.com>
2020-04-21 14:38:21 +0100

    Added a CandidateLink component for each list item.
    Added some styling and some dummy data for now.

---
+++ components/CandidateLink/CandidateLink.tsx
@@ -0,0 +1,19 @@
+﻿import React from "react";
+import textStyles from './CandidateLink.module.scss';
+
+
+
+interface CandidateLinkProps {
+    name : string;
+}
+
+
+export function CandidateLink(props: CandidateLinkProps): JSX.Element {
+
+    return (
+        <li className={textStyles.listItem}>
+            {props.name}
+            <hr/>
+        </li>
+    )
+}
--- components/CandidateList/CandidateList.tsx
+++ components/CandidateList/CandidateList.tsx
@@ -1,5 +1,8 @@
 ﻿import React from "react";
 import scss from './CandidateList.module.scss';
+import textStyles from '../../pageStyles/text-classes.module.scss'
+import {getCandidates} from "../../api/candidatesApiClient";
+import {CandidateLink} from "../CandidateLink/CandidateLink";
 
 
 interface CandidateListProps {
@@ -9,9 +12,14 @@ interface CandidateListProps {
 
 export function CandidateList(props: CandidateListProps): JSX.Element {
     
+    
+    const candidates = ["Michael Walker", "Stephen Jackson"];
+    
     return (
-        <div className={scss.test}>
-            Hello
-        </div>
+        <ul className={scss.list}>
+            {
+                candidates.map(name => <CandidateLink name={name}/>)
+            }
+        </ul>
     )
 }
