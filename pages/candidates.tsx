408cde053f2e8d9a91c960b4f3881924582a4ec8
Jamie Baird <james.baird@softwire.com>
2020-04-21 13:56:46 +0100

    Added common text-styles stylesheet

--- pages/candidates.tsx
+++ pages/candidates.tsx
@@ -1,14 +1,19 @@
 ﻿import React from "react";
 import {NextPage} from "next";
-import scss from "../pageStyles/candidates.module.scss"
+import textStyle from "../pageStyles/text-classes.module.scss"
 import Layout from "../components/Layout/layout";
 import {CandidateList} from "../components/CandidateList/CandidateList";
 
+const Candidates: NextPage = () => {
 
-const Candidates: NextPage = () =>
-    <Layout>
-        <h1 className={scss.h1}>Candidates</h1>
-        <CandidateList/>
-    </Layout>;
+    return (
+        <Layout>
+            <h1 className={textStyle.pageHeader}>Candidate Manager</h1>
+            <h2 className={textStyle.subHeader}> Select a Candidate...</h2>
+            <hr className={textStyle.lineBreak}/>
+            <CandidateList/>
+        </Layout>
+    );
+};
 
 export default Candidates;
