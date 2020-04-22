d45eea9c1186ce97169fcafbef3b0798169ac7d6
Jamie Baird <james.baird@softwire.com>
2020-04-22 13:20:41 +0100

    Linter edits
    -Moved function above where it is first called
    -Removed unnecessary type for string literal
    -changed let to const

--- api/loginApiClient.module.ts
+++ api/loginApiClient.module.ts
@@ -1,4 +1,13 @@
-﻿export async function apiTryLogin(userId: string, password: string): Promise<boolean> {
+﻿const API_URL = "https://testswitch-api-staging.herokuapp.com/candidates"; //TODO change to /signin when endpoint exists
+
+async function loginCredentialsIncorrect(): Promise<boolean>{
+    const response = await fetch(API_URL);
+    return response.status == 200;
+
+    //TODO add if(response.status == 403) then not authorized, anything else is another issue.
+}
+
+export async function apiTryLogin(userId: string, password: string): Promise<boolean> {
     
     if (password == "") {
         alert("You did not enter a password! please enter your password.");
@@ -15,13 +24,4 @@
     else{
         return true;
     }
-}
-
-const API_URL: string = "https://testswitch-api-staging.herokuapp.com/candidates"; //TODO change to /signin when endpoint exists
-
-async function loginCredentialsIncorrect(): Promise<boolean>{
-    let response = await fetch(API_URL);
-    return response.status == 200;
-    
-    //TODO add if(repsonse.status == 403) then not authorized, anything else is another issue.
 }
