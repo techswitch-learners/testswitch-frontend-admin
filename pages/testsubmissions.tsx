67413ae90aea5d6a6038a72bc08cdbb49cfbfb9a
hnalabanda <hemanalabanda@gmail.com>
2020-04-23 10:04:56 +0100

    added the page for test submissions

---
+++ pages/testsubmissions.tsx
@@ -0,0 +1,19 @@
+﻿import React from "react";
+import {NextPage} from "next";
+import textStyle from "../pageStyles/text-classes.module.scss"
+import Layout from "../components/Layout/layout";
+
+
+const TestSubmissions: NextPage = () => {
+
+    return (
+        <Layout>
+            <h1 className={textStyle.pageHeader}>Test<br/>Submissions</h1>
+            <h2 className={textStyle.subHeader}> Select a Candidate...</h2>
+            <hr className={textStyle.lineBreak}/>
+            
+        </Layout>
+    );
+};
+
+export default TestSubmissions;
