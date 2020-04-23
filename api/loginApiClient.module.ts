99bfd0ebf7b0e98ab4e93e094f2993b36fb88baf
Jamie Baird <james.baird@softwire.com>
2020-04-23 10:32:32 +0100

    Put all client side checks in component, renamed all functions

--- api/loginApiClient.module.ts
+++ api/loginApiClient.module.ts
@@ -1,36 +1,17 @@
 ﻿const API_URL = "https://testswitch-api-staging.herokuapp.com/candidates"; //TODO change to /signin when endpoint exists
 
-function clientSideValidation(userId: string, password: string): boolean {
-    
-    if (password == "") {
-        alert("You did not enter a password! please enter your password.");
-        return false;
-    } 
-    if (userId == "") {
-        alert("You did not enter a User ID! Please enter your user ID.");
-        return false;
-    }
-    return true;
-}
-
-async function tryLoginWithApi(userId: string, password: string): Promise<number>{
+async function checkCredentialsInApi(userId: string, password: string): Promise<number>{
     //TODO: delete test constant and return actual status code from api
     // const response = await fetch(API_URL); etc
     // const statusCode = response.status;
     
     //Check how the page works with 200, 403, and any other error code.
-    return 403;
+    return 200;
 }
 
-export async function isLoginAttemptValid(userId: string, password: string): Promise<boolean> {
+export async function validateCredentialsInApi(userId: string, password: string): Promise<boolean> {
     
-    const credentialsValidatedByClient = clientSideValidation(userId, password);
-    
-    if(!credentialsValidatedByClient){
-        return false;
-    }
-    
-    const statusCodeFromServer = await tryLoginWithApi(userId, password);
+    const statusCodeFromServer = await checkCredentialsInApi(userId, password);
     
     if(statusCodeFromServer == 403){
         alert("Those details are not in our system! Please try again");
--- components/LoginForm/LoginForm.tsx
+++ components/LoginForm/LoginForm.tsx
@@ -1,4 +1,4 @@
-﻿import {isLoginAttemptValid} from "../../api/loginApiClient.module";
+﻿import {validateCredentialsInApi} from "../../api/loginApiClient.module";
 import React, {FormEvent, useState} from "react";
 import scss from "./LoginForm.module.scss";
 import {useRouter} from "next/router";
@@ -11,13 +11,28 @@ interface LoginFormProps {
 }
 
 export function LoginForm(props: LoginFormProps): JSX.Element {
-    
+
     const {userId, password, setUserId, setPassword} = props;
     const router = useRouter();
 
+    async function clientSideValidation(userId: string, password: string): Promise<boolean> {
+
+        if (password == "") {
+            alert("You did not enter a password! please enter your password.");
+            return false;
+        }
+        if (userId == "") {
+            alert("You did not enter a User ID! Please enter your user ID.");
+            return false;
+        }
+        return true;
+    }
+
     function tryLogin(event: FormEvent): void {
-        isLoginAttemptValid(props.userId, props.password)
-            .then(canBeSubmitted => canBeSubmitted ? router.push('/candidates'): {});
+        clientSideValidation(props.userId, props.password)
+            .then(canBeCheckedInApi => canBeCheckedInApi ?
+                validateCredentialsInApi(props.userId, props.password) : false)
+            .then(canBeSubmitted => canBeSubmitted ? router.push('/candidates') : {});
         event.preventDefault();
     }
 
