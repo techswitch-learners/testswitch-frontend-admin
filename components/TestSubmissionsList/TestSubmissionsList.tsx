3604c689f417357134021a119bc5f78d67383dc6
Beatrice <beatrice.opara@softwire.com>
2020-04-23 12:58:56 +0100

    Add list of test submissions
    Added link to each test submissions

---
+++ components/TestSubmissionsList/TestSubmissionsList.tsx
@@ -0,0 +1,19 @@
+﻿import React from "react";
+import scss from './TestSubmissionsList.module.scss';
+import {TestSubmissionsLink} from "../TestSubmissionsLink/TestSubmissionsLink";
+
+
+export function TestSubmissionsList(): JSX.Element {
+    
+    //TODO: getTestSubmissions result from API
+    
+    const tests: string [] = ["Test 1, Test 2"];
+    
+    return (
+        <ul className={scss.list}>
+            {
+                tests.map(test => <TestSubmissionsLink key={tests.indexOf(test)} test={test}/>)
+            }
+        </ul>
+    )
+}
--- pages/testsubmissions.tsx
+++ pages/testsubmissions.tsx
@@ -2,16 +2,17 @@
 import {NextPage} from "next";
 import textStyle from "../pageStyles/text-classes.module.scss"
 import Layout from "../components/Layout/layout";
-
+import {TestSubmissionsList} from "../components/TestSubmissionsList/TestSubmissionsList";
 
 const TestSubmissions: NextPage = () => {
 
     return (
         <Layout>
-            <h1 className={textStyle.pageHeader}>Test<br/>Submissions</h1>
-            <h2 className={textStyle.subHeader}> Select a Candidate...</h2>
+            <h1 className={textStyle.pageHeader}>Test Submissions</h1>
+            <h2 className={textStyle.subHeader}>Candidate Name</h2>
             <hr className={textStyle.lineBreak}/>
-            
+            <h2 className={textStyle.subHeader}>Submissions</h2>
+            <TestSubmissionsList/>
         </Layout>
     );
 };
