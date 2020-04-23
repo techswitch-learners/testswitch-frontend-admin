c385ac3b507da225bf06be53134a2c411a8be0ef
Beatrice <beatrice.opara@softwire.com>
2020-04-23 10:42:59 +0100

    added test submission link page

---
+++ components/TestSubmissionLink/TestSubmissionLink.tsx
@@ -0,0 +1,18 @@
+﻿import React from "react";
+import textStyles from "../CandidateLink/CandidateLink.module.scss";
+import Link from "next/link";
+
+interface TestSubmissionLinkProps {
+    name: string;
+}
+
+export function TestSubmissionLink(props: TestSubmissionLinkProps) : JSX.Element {
+    
+    const ref: string = "/candidates/test-submission" + props.name; 
+   
+    return (
+        <li>
+           
+        </li>
+    )
+}
