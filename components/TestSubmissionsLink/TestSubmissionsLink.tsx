643b3fc04665c80064383a457c64a561cbe85e57
Beatrice <beatrice.opara@softwire.com>
2020-04-24 09:34:29 +0100

    cleaning up code
    removed unused links, styles

--- components/TestSubmissionsLink/TestSubmissionsLink.tsx
+++ components/TestSubmissionsLink/TestSubmissionsLink.tsx
@@ -1,22 +1,17 @@
 ﻿import React from "react";
 import textStyles from './TestSubmissionsLink.module.scss';
-import Link from "next/link";
 
 interface TestSubmissionsLinkProps {
     test: string;
 }
 
-export function TestSubmissionsLink(props: TestSubmissionsLinkProps) : JSX.Element {
-    
-    const ref: string = "/test-submissions/"; 
+export function TestSubmissionsLink(props: TestSubmissionsLinkProps): JSX.Element {
    
     //TODO: how will results appear to admin? for now put textarea as placeholder
 
     return (
         <li className={textStyles.listItem}>
-            <Link href={ref}>
                 <p className={textStyles.link}>{props.test}</p>
-            </Link>
             <textarea className={textStyles.textArea}>
                 Placeholder for Test Results
             </textarea>
