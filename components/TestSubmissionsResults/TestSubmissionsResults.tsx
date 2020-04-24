8979c40a5d81e9fa71a88cfdd47b5bf8ed0d341e
Beatrice <beatrice.opara@softwire.com>
2020-04-24 12:36:04 +0100

    recreated test submission branch
    cleaner branch with approved merges

---
+++ components/TestSubmissionsResults/TestSubmissionsResults.tsx
@@ -0,0 +1,21 @@
+﻿import React from "react";
+import textStyles from './TestSubmissionsResults.module.scss';
+
+interface TestSubmissionsResultsProps {
+    test: string;
+}
+
+export function TestSubmissionsResults(props: TestSubmissionsResultsProps): JSX.Element {
+   
+    //TODO: how will results appear to admin? for now put textarea as placeholder
+
+    return (
+        <li className={textStyles.listItem}>
+                <p>{props.test}</p>
+            <textarea className={textStyles.textArea}>
+                Placeholder for Test Results
+            </textarea>
+        </li>
+    );
+}
+
