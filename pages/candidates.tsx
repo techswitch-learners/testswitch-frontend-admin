06ff1b33a37bb109b0d890be932a46b46474d731
Jamie Baird <james.baird@softwire.com>
2020-04-27 10:08:54 +0100

    Added two buttons (one for desktop, one for mobile)
    Preliminary styling - checking with Sasha to confirm

--- pages/candidates.tsx
+++ pages/candidates.tsx
@@ -1,14 +1,18 @@
 ﻿import React from "react";
 import {NextPage} from "next";
-import textStyle from "../pageStyles/text-classes.module.scss"
+import textStyle from "../pageStyles/text-classes.module.scss";
+import scss from "../pageStyles/candidates.module.scss";
 import Layout from "../components/Layout/layout";
 import {CandidateList} from "../components/CandidateList/CandidateList";
+import Link from "next/link";
 
 const Candidates: NextPage = () => {
 
     return (
         <Layout>
             <h1 className={textStyle.pageHeader}>Candidate Manager</h1>
+            <button className={scss.desktopButton}><Link href={"/create"}><p className={scss.link}>New Candidate</p></Link></button>
+            <button className={scss.mobileButton}><Link href={"/create"}><p className={scss.link}>+</p></Link></button>
             <h2 className={textStyle.subHeader}> Select a Candidate...</h2>
             <hr className={textStyle.lineBreak}/>
             <CandidateList/>
