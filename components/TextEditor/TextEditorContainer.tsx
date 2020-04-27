8c9ee63e4979ff68305af9dcc9de2edabd9ff8ae
Beatrice <beatrice.opara@softwire.com>
2020-04-27 12:21:13 +0100

    added text container component

---
+++ components/TextEditor/TextEditorContainer.tsx
@@ -0,0 +1,27 @@
+﻿import React from "react";
+import Editor from "react-monaco-editor";
+import {TextEditorSettings} from "./TextEditorSettings";
+
+interface TextEditorContainerProps {
+    theme?: string;
+    height: string;
+    width: string;
+    defaultText: string;
+}
+
+export function TextEditorContainer(props: TextEditorContainerProps): JSX.Element {
+   return (
+       <div>
+           <Editor 
+           theme={props.theme}
+           height={props.height}
+           width={props.width}
+           language='javascript'
+           value={props.defaultText}
+           options={TextEditorSettings}
+           />
+       </div>
+   )
+}
+
+export default TextEditorContainer;
