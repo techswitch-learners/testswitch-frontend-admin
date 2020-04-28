4d4fcd1bb9a03130500b13fc3a5b9fee790b6d8e
Jamie Baird <james.baird@softwire.com>
2020-04-28 14:42:49 +0100

    Added environment variable into next.config.js file
    -need to create a .env file in own directory with this variable:
    
    SETAPIURL="https://localhost:5001"

--- next.config.js
+++ next.config.js
@@ -2,3 +2,14 @@
 module.exports = withSass({
     cssModules: true
 });
+
+module.exports = () => {
+    const env = {
+        APIURL: process.env.SETAPIURL,
+    };
+    return {
+        env,
+    }
+};
+
+
