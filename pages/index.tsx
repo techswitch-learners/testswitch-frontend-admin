d9325b507dc98ca4d02069bf96c15476dab96a72
Jamie Baird <james.baird@softwire.com>
2020-04-29 15:53:31 +0100

    Added Runtime config to next.config.js and display url on homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,14 @@
 import {NextPage} from "next";
 import scss from "../pageStyles/index.module.scss"
 import Layout from "../components/Layout/layout";
+import getConfig from 'next/config';
 
+
+const { publicRuntimeConfig } = getConfig();
 
 const Home: NextPage = () => 
     <Layout>
-        <h1 data-testid="page-render-test" className={scss.example}>Hello Admin</h1>
+        <h1 data-testid="page-render-test" className={scss.example}>Hello {publicRuntimeConfig.API_URL}</h1>
     </Layout>;
 
 export default Home;
