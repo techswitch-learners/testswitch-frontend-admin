f0845797428b8b1783f54c15f58002642e4d343b
Wendy <alteredco@gmail.com>
2020-05-01 10:32:25 +0100

    oops! updated to publicRuntime config again!

--- api/candidatesApiClient.ts
+++ api/candidatesApiClient.ts
@@ -19,8 +19,7 @@ export interface Candidate {
 
 export async function getCandidates(page: number, pageSize: number): Promise<ListResponse<Candidate>> {
     const {publicRuntimeConfig} = getConfig();
-    /*const apiURL = publicRuntimeConfig.API_URL;*/
-    const apiURL = `https://testswitch-api-staging.herokuapp.com`;
+    const apiURL = publicRuntimeConfig.API_URL;
     const response = await fetch(`${apiURL}/candidates?page=${page}&pageSize=${pageSize}`);
 
     if (response.ok) {
@@ -32,8 +31,7 @@ export async function getCandidates(page: number, pageSize: number): Promise<Lis
 
 export async function getCandidateById(cid: number): Promise<Candidate> {
     const {publicRuntimeConfig} = getConfig();
-    /*const apiURL = publicRuntimeConfig.API_URL;*/
-    const apiURL = `https://testswitch-api-staging.herokuapp.com`;
+    const apiURL = publicRuntimeConfig.API_URL;
     const response = await fetch(`${apiURL}/candidates/${cid}`);
     if (response.ok) {
         const candidate = await response.json();
