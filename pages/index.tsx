453c2797fbd0a75b85e807907875bcf447fb5a73
Jamie Baird <james.baird@softwire.com>
2020-05-04 14:48:12 +0100

    Added redirect to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,20 @@ import {NextPage} from "next";
 import scss from "../pageStyles/index.module.scss"
 import Layout from "../components/Layout/layout";
 
-const Home: NextPage = () => 
+const Home: NextPage = () =>
     <Layout>
         <h1 data-testid="page-render-test" className={scss.example}>Hello Admin Site</h1>
     </Layout>;
+
+Home.getInitialProps = ({res}) => {
+
+    if (res) {
+        res.writeHead(301, {
+            Location: 'candidates'
+        });
+        res.end();
+    }
+    return {};
+};
 
 export default Home;
