7e515c233539e9b551c11f795fc31b30c3af04d9
Wendy <alteredco@gmail.com>
2020-05-06 17:52:52 +0100

    add GUID_URL conditions to next.config based on NODE_ENV and update the CandidateGuid component

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ module.exports = withSass({
     cssModules: true,
     publicRuntimeConfig: {
         API_URL: process.env.API_URL,
+        GUID_URL: (process.env.NODE_ENV==='staging'||'development')?(`https://testswitch-candidate-staging.herokuapp.com`):(`https://testswitch-candidate.herokuapp.com`),
     },
 });
 
