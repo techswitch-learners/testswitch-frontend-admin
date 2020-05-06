71d6d01dbdd80d95b741ff3ff5afbc24d47cf99d
Wendy <alteredco@gmail.com>
2020-05-06 15:54:03 +0100

    add button leading to log in page on index and style index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,17 @@
 ﻿import React from "react";
 import {NextPage} from "next";
 import scss from "../pageStyles/index.module.scss"
+import Link from "next/link";
 import Layout from "../components/Layout/layout";
+import textStyle from "../pageStyles/text-classes.module.scss";
 
 const Home: NextPage = () => 
     <Layout>
-        <h1 data-testid="page-render-test" className={scss.example}>Hello Admin Site</h1>
+        <h1 data-testid="page-render-test" className={scss.example}>Welcome Admins</h1>
+        <hr className={textStyle.lineBreak}/>
+        <div className={scss.linkBtnContainer}>
+            <Link href={"/login"}><a className={scss.linkBtn}>Go To Log in</a></Link>
+        </div>
     </Layout>;
 
 export default Home;
