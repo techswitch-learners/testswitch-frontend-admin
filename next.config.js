77118d4b5255d671b59ab7cd0a6ef31920f93257
Wendy <alteredco@gmail.com>
2020-05-07 11:03:38 +0100

    update GUID_URL and add env vars

--- next.config.js
+++ next.config.js
@@ -3,8 +3,17 @@ module.exports = withSass({
     cssModules: true,
     publicRuntimeConfig: {
         API_URL: process.env.API_URL,
-        GUID_URL: (process.env.NODE_ENV==='staging'||'development')?(`https://testswitch-candidate-staging.herokuapp.com`):(`https://testswitch-candidate.herokuapp.com`),
+        GUID_URL: process.env.CANDIDATE_SITE_URL,
     },
+    
+    env: {
+        CANDIDATE_SITE_URL: "https://testswitch-candidate-staging.herokuapp.com",
+    },
+    build: {
+        env: {
+            CANDIDATE_SITE_URL: "https://testswitch-candidate.herokuapp.com",
+        }
+    }
 });
 
 
