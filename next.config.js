9afe983ba507381b9e4f501cf9225d611f5d6211
Wendy <alteredco@gmail.com>
2020-05-07 11:56:36 +0100

    remove env var from next.config

--- next.config.js
+++ next.config.js
@@ -5,15 +5,6 @@ module.exports = withSass({
         API_URL: process.env.API_URL,
         GUID_URL: process.env.CANDIDATE_SITE_URL,
     },
-    
-    env: {
-        CANDIDATE_SITE_URL: "https://testswitch-candidate-staging.herokuapp.com",
-    },
-    build: {
-        env: {
-            CANDIDATE_SITE_URL: "https://testswitch-candidate.herokuapp.com",
-        }
-    }
 });
 
 
